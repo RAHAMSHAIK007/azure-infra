d09531d10f58ee31781304467fb28a8708f06d2d
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2026-03-17 07:56:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,17 @@ provider "azurerm" {
 }
 
 
+terraform {
+  backend "azurerm" {
+    use_oidc             = true                                    # Can also be set via `ARM_USE_OIDC` environment variable.
+    use_azuread_auth     = true                                    # Can also be set via `ARM_USE_AZUREAD` environment variable.
+    storage_account_name = "terraformproj9948113498"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "terraformproj"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+}
+
+
 resource "azurerm_resource_group" "example" {
   name     = "demo-resources"
   location = "UK South"
