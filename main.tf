a8570e125c3cab75ea30b03fea88f0f46936f9e1
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2026-03-17 07:38:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname-demoinfra-0099"
+  name                     = "bootdemoinfra0099"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
